3548e37591709d2ceb629f68ecf362ff6be5f5b2
Joany Mariño <jmarinocoron@mun.ca>
2021-11-10 16:18:25 +0100

    add ExPanDaR code and report

---
+++ ExPanDaR/run-report_ExPanDaR.R
@@ -0,0 +1,22 @@
+# ExPanDaR SHIP analysis
+# https://joachim-gassen.github.io/ExPanDaR/
+# https://joachim-gassen.github.io/ExPanDaR/articles/ExPanD_notebook.html
+
+if (!require(ExPanDaR)) install.packages("ExPanDaR")
+
+# Load packages ----
+library(ExPanDaR)
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Load metadata ----
+md1 <- readRDS(system.file("extdata", "ship_meta.RDS", package = "dataquieR"))
+
+# Run report ----
+ExPanD(df = sd1,  
+       df_name = "SHIP",
+       export_nb_option = TRUE)
+
