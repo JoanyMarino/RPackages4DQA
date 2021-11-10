a79fce21e20aa175a9c240a9849cf6671a5358d4
Joany Mariño <jmarinocoron@mun.ca>
2021-11-10 18:26:12 +0100

    add explore code and report

---
+++ explore/run-report_explorer.R
@@ -0,0 +1,42 @@
+# explore SHIP analysis
+# https://github.com/rolkra/explore
+# https://cran.r-project.org/web/packages/explore/vignettes/explore.html
+
+if (!require(explore)) install.packages("explore")
+if (!require(magrittr)) install.packages("magrittr")
+
+# Load packages ----
+library(explore)
+library(magrittr)  # to use the pipe operator %>%
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Interactive exploration ----
+explore(sd1)
+
+# Run report ----
+sdq %>% report(output_dir = gewd())
+
+# Manual exploration ----
+# explore by variable
+sd1 %>% explore(age)
+sd1 %>% explore(sbp1)
+
+# explore relationship
+sd1 %>% explore(sbp1, target = dbp1)
+
+# explore relationship between all variables and the target
+# sd1 %>% explore_all(target = sbp1)
+
+# explore correlation between Sepal.Length and Petal.Length
+sd1 %>% explore(sbp1, dbp1)
+
+# describe dataset
+describe(sd1)
+
+# describe variables
+sd1 %>% describe(age)
+sd1 %>% describe(sbp1)
