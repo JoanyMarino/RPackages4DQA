1df520ce6fcc92d35a105bd195018f06eb14c957
Joany Mariño <jmarinocoron@mun.ca>
2021-11-18 16:19:51 +0100

    add assertr + pointblank scripts

---
+++ assertr/run_analysis_assertr.R
@@ -0,0 +1,48 @@
+# assertr SHIP analysis
+# https://docs.ropensci.org/assertr/articles/assertr.html
+
+if (!require(assertr)) install.packages("assertr")
+if (!require(dplyr)) install.packages("dplyr")
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Custom checks
+
+check_assertions <- . %>%
+  chain_start(store_success = TRUE) %>%
+  # Unexpected data elements
+  verify(nrow(.) == 2154) %>%
+  # Duplicates
+  assert_rows(duplicates_across_cols, in_set(FALSE), id) %>%
+  # Data type mismatch
+  verify(has_class("id", class = "integer")) %>%
+  verify(has_class("sex", class = "integer")) %>%
+  verify(has_class("sbp1", class = "integer")) %>%
+  verify(has_class("sbp2", class = "integer")) %>%
+  verify(has_class("weight", class = "numeric")) %>%
+  # verify(has_class("waist", class = "numeric"), error_fun = defect_df_return) %>%
+  # Value format 
+  verify(has_all_names("sbp1", "sbp2", "weight")) %>%
+  # Missingness
+  assert_rows(num_row_NAs, within_bounds(0,1), everything()) %>%
+  # Numerical values
+  # Inadmissible (hard limits)
+  assert(within_bounds(80, 200), c("sbp1", "sbp2")) %>%
+  # Uncertain (soft limits)
+  assert(within_bounds(90, 180, include.lower = FALSE, include.upper = FALSE), c("sbp1", "sbp2")) %>%
+  # Categorical values (and standardized vocabulary)
+  assert(in_set(seq(0, 4)), "school") %>%
+  # Outliers
+  insist(within_n_sds(4), "sbp1") %>%
+  # verify(has_all_names("mpgg"))  , error_fun = warn_report
+  chain_end(success_fun=success_df_return) 
+
+# Run analysis ----
+
+sd1 %>%
+  check_assertions #%>%
+  # group_by(cyl) %>%
+  # summarise(avg.mpg=mean(mpg))
---
+++ pointblank/run-report_pointblank.R
@@ -0,0 +1,47 @@
+# pointblank SHIP report
+# https://joachim-gassen.github.io/ExPanDaR/
+# https://joachim-gassen.github.io/ExPanDaR/articles/ExPanD_notebook.html
+
+if (!require(pointblank)) install.packages("pointblank")
+if (!require(ggforce)) install.packages("ggforce")
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Run reports ----
+
+# Table Scan Workflow (VALID-V) ----
+scan_data(sd1)
+
+# Data Quality Reporting Workflow (VALID-I) ----
+al <- action_levels(notify_at = 0.1, warn_at = 0.2, stop_at = 0.5)
+
+agent <- 
+  create_agent(
+    tbl = sd1,
+    tbl_name = "SHIP data",
+    label = "SHIP Data Quality Report (VALID-I)",
+    actions = al
+  ) %>%
+  # Data elements
+  col_exists(vars(id, sex, sbp1, sbp2)) %>%
+  # Duplicates
+  rows_distinct(vars(id, sex, sbp1, sbp2)) %>%
+  # Type
+  col_is_integer(vars(id, sex, sbp1, sbp2)) %>%
+  col_is_numeric(vars(weight, waist)) %>%
+  col_is_date(vars(exdate)) %>%
+  # Missingness
+  col_vals_not_null(vars(sbp1, sbp2)) %>%
+  # Time-date values (inadmissible and uncertain)
+  # Categorical values
+  col_vals_in_set(vars(school), set = seq(0, 4)) %>%
+  # Numerical values (inadmissible and uncertain)
+  col_vals_between(vars(sbp1), left = 90, right = 80) %>%
+  interrogate()
+
+agent
+# get_agent_report(agent)
+
