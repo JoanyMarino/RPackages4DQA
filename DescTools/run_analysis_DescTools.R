3ddc248592231a769fc5db12807e0a724ad74e94
Joany Mariño <jmarinocoron@mun.ca>
2022-02-01 12:58:24 +0100

    add DescTools

---
+++ DescTools/run_analysis_DescTools.R
@@ -0,0 +1,26 @@
+# DescTools SHIP analysis
+# https://cran.r-project.org/package=DescTools
+
+if (!require(DescTools)) install.packages("DescTools")
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Run analysis ----
+
+# Overview
+Abstract(sd1)
+Desc(sd1)
+
+# Missings
+PlotMiss(sd1, main="Missing data", clust = TRUE) 
+
+# Correlation
+m <- cor(sd1[,which(sapply(sd1, is.numeric))], use="pairwise.complete.obs")
+
+PlotCorr(m, border="grey",
+         args.colorlegend=list(labels=Format(seq(1,-1,-.25), 2), frame="grey"))
+  
+PlotWeb(m, col=c(hred, hblue))
