2ca9fdeb83a68cb7df2fde4d3637396963b27ed9
Joany Mariño <jmarinocoron@mun.ca>
2022-02-02 17:10:41 +0100

    add mdapack

---
+++ mdapack/run_analysis_mdapack.R
@@ -0,0 +1,26 @@
+# mdapack SHIP analysis
+# https://cran.r-project.org/package=mdapack
+
+if (!require(mdapack)) install.packages("mdapack")
+
+# Load data ----
+
+# Study of Health in Pomerania
+sd1 <- readRDS(system.file("extdata", "ship.RDS", package = "dataquieR"))
+
+# Run analysis ----
+
+# Integrity
+checkValidity(sd1)
+checkVariableNames(sd1, c("age","sex", "id", "sbp1", "sbp2"))
+
+# Completeness
+missingAnalysis(sd1, miss_pattern = TRUE)
+missingAnalysis(sd1, plot = TRUE)
+missingAnalysis(sd1, miss_cor_pattern = TRUE)
+
+# Accuracy
+outlier_detection(sd1)
+univariateAnalysis(sd1, hist = TRUE)
+univariateAnalysis(sd1$age, hist = TRUE)
+plotHeatmapGroup(sd1[,11:14])
