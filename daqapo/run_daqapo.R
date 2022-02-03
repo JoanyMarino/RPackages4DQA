1def01947179e589b430fd547358cb27d514eba5
kasbohme <kasbohme@uni-greifswald.de>
2022-02-03 11:36:42 +0000

    add daqapo stub

---
+++ daqapo/run_daqapo.R
@@ -0,0 +1,5 @@
+if (!require(here)) install.packages("here") # package for relative paths
+
+if (!require(daqapo)) install.packages("daqapo")
+
+# our data is neither an activity log nor an event log :/
