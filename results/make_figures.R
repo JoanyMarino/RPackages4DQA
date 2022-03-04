df42f38588882a3ef5d9a82a4e688964ff544f61
Joany Mariño <jmarinocoron@mun.ca>
2022-03-04 13:58:11 +0100

    update plotting scripts

--- results/make_figures.R
+++ results/make_figures.R
@@ -8,15 +8,16 @@ library(readxl)
 
 # REPLACE PATH TO SPREADSHEET
 # TO DO: Move file to git repo
-dq_table <- read_excel("~/Documents/2021-10_DataQualityToolsReviewPaper/DQ_paper_table_v06.xlsx")
+dq_table <- read_excel("~/Documents/2021-10_DataQualityToolsReviewPaper/DQ_paper_table_v06_indicators.xlsx")
 
 # Remove last rows
-# dq_table <- dq_table[-c(24, 25),]
+dq_table <- dq_table[-seq(25, nrow(dq_table)),]
+dq_table <- subset(dq_table, select=-c(analyzer, discoveR))
 
 
 # Split tables
-dq_broad_wide <- dq_table[c(1:11),]
-dq_domains_wide <- dq_table[c(12:22),]
+dq_broad_wide <- dq_table[c(1:14),]
+dq_domains_wide <- dq_table[c(15:24),]
 
 # Figure 2: Broad criteria ----
 
@@ -24,7 +25,7 @@ dq_domains_wide <- dq_table[c(12:22),]
 dq_long <- dq_broad_wide %>%  
   pivot_longer(
     # CHANGE cols TO MATCH ORDER IN SPREADSHEET
-    cols = analyzer:xray,
+    cols = assertable:xray,
     names_to = c("Package"),
     values_to = "Feature",
     values_drop_na = FALSE) %>% 
@@ -40,18 +41,20 @@ dq_broad <- dq_long %>%
 
 # Order factors for plotting
 dq_broad$Criteria <- factor(dq_broad$Criteria, 
-                            levels = rev(c("Reference to data quality", 
-                                           "Reference to data quality concept", 
+                            levels = rev(c("Explicit reference to data quality", 
+                                           "Based on a data quality framework", 
                                            "Control via GUI",
-                                           "Control via programming",
-                                           "Requires programming",
-                                           "Report generation from single function",
+                                           "Reproducibility via programming",
+                                           "Single function for output",
+                                           "Report generation (not only console)",
                                            "Input of metadata through functions",
                                            "Input of metadata through separate file",
-                                           "Management of metadata",
                                            "Grading of data quality issues",
-                                           "Data summary/overview",
-                                           "Descriptive statistics")))
+                                           "Data set summary/overview",
+                                           "Descriptive summary statistics",
+                                           "Descriptive statistics graphs (univariate)",
+                                           "Descriptive statistics graphs (multivariate)",
+                                           "Handles string properties")))
 
 dq_broad$Feature <- factor(dq_broad$Feature, 
                            levels = c("Incorporated", "Not incorporated"))
@@ -64,8 +67,8 @@ fig2 <- ggplot(dq_broad,
   geom_text(position = position_fill(vjust = 0.5),
             colour = "white", 
             size = rel(5.5)) +
-  scale_fill_manual(values = c("Incorporated" = "#E59100", 
-                               "Not incorporated" = "#005191")) +
+  scale_fill_manual(values = c("Incorporated" = "#56B4E9", 
+                               "Not incorporated" = "#E69F00")) +
   coord_flip() + 
   labs(y = "No. of Domains")  +
   theme_classic() +
@@ -86,13 +89,14 @@ fig2 <- ggplot(dq_broad,
 dq_domains_long <- dq_domains_wide %>%  
   pivot_longer(
     # CHANGE cols TO MATCH ORDER IN SPREADSHEET
-    cols = analyzer:xray,
+    cols = assertable:xray,
     names_to = c("Package"),
     values_to = "Feature",
     values_drop_na = FALSE) %>% 
   mutate(Criteria = factor(Criteria),
          Package = factor(Package), 
-         Feature = case_when(!is.na(Feature) ~ "Incorporated", 
+         Feature = case_when(!str_detect(Feature, "d") ~ "Indicator", 
+                             str_detect(Feature, "d") ~ "Descriptor", 
                              # else, no
                              TRUE ~ "Not incorporated")) 
 
@@ -112,11 +116,10 @@ dq_domains$Criteria <- factor(dq_domains$Criteria,
                                              "Consistency: Contradictions",
                                              "Accuracy: Unexpected distribution",
                                              "Accuracy: Unexpected association",
-                                             "Accuracy: Disagreement of rep. meas.",
-                                             "Accuracy: Loners")))
+                                             "Accuracy: Disagreement of rep. meas.")))
 
 dq_domains$Feature <- factor(dq_domains$Feature, 
-                             levels = c("Incorporated", "Not incorporated"))
+                             levels = c("Indicator", "Descriptor", "Not incorporated"))
 
 fig3 <- ggplot(dq_domains, 
                aes(fill = forcats::fct_rev(Feature), y = n, x = Criteria, label = n)) +
@@ -126,8 +129,9 @@ fig3 <- ggplot(dq_domains,
   geom_text(position = position_fill(vjust = 0.5),
             colour = "white", 
             size = rel(5.5)) +
-  scale_fill_manual(values = c("Incorporated" = "#E59100", 
-                               "Not incorporated" = "#005191")) +
+  scale_fill_manual(values = c("Indicator" = "#56B4E9", 
+                               "Descriptor" = "#0072B2", 
+                               "Not incorporated" = "#E69F00")) +
   coord_flip() + 
   # scale_x_discrete(limits = rev(levels(dq_broad$Criteria))) +
   labs(y = "No. of Domains")  +
@@ -143,5 +147,5 @@ fig3 <- ggplot(dq_domains,
         legend.text = element_text(size=rel(1.3))
   )
 
-ggsave("figs/fig2_v4.pdf", fig2, width = 12, height = 6, units = "in", dpi = 400)
-ggsave("figs/fig3_v4.pdf", fig3, width = 12, height = 6, units = "in", dpi = 400)
+ggsave("figs/fig2_v5.pdf", fig2, width = 12, height = 6, units = "in", dpi = 400)
+ggsave("figs/fig3_v5.pdf", fig3, width = 12, height = 6, units = "in", dpi = 400)
--- results/make_figures_pkgs.R
+++ results/make_figures_pkgs.R
@@ -8,20 +8,30 @@ library(ggpubr)
 
 # REPLACE PATH TO SPREADSHEET
 # TO DO: Move file to git repo
-pkgs <- read_excel("~/Documents/2021-10_DataQualityToolsReviewPaper/DQ_packages_dimensions.xlsx")
+pkgs <- read_excel("~/Documents/2021-10_DataQualityToolsReviewPaper/DQ_packages_dimensions_v2.xlsx")
+
+pkgs <- subset(pkgs, select=-c(analyzer, discoveR))
+
+# str_count(test, '(^|,)[0-9]+d(,|$)')
+# str_count(test, '(^|,)[0-9]+[A-Z]?(,|$)')
 
 pkgs_long <- pkgs %>%  
   mutate(across(xplorerr, as.character)) %>% 
   pivot_longer(
-    cols = analyzer:xray,
+    cols = assertable:xray,
     names_to = c("Package"),
     values_to = "Feature",
     values_drop_na = TRUE) %>% 
   mutate(Dimension = factor(Dimension),
          Domain = factor(Domain),
-         Package = factor(Package)) %>%  
+         Package = factor(Package)) %>%
+  # remove spaces
+  mutate(Feature = gsub("\\s+", "", Feature, perl = TRUE)) %>%
   # Count features by package
-  mutate(Count = str_count(Feature, '[0-9.]+')) %>%
+  mutate(Descriptors = str_count(Feature, '(^|,)[0-9]+d(,|$)'),
+         Indicators = str_count(Feature, '(^|,)[0-9]+[A-Z]?(,|$)')) %>%
+  # mutate(Descriptors = replace_na(Descriptors, 0),
+  #        Indicators = replace_na(Indicators, 0)) %>%
   # Reorder dimension levels for plot 
   mutate(Dimension = fct_relevel(Dimension, 
                               "Integrity", 
@@ -38,15 +48,104 @@ pkgs_long <- pkgs %>%
                               "Range and value violations",
                               "Contradictions",
                               "Unexpected distribution",
-                              "Unexpected association",
-                              "Disagreement of rep. meas.", 
-                              "Loners")) 
+                              "Unexpected association"
+                              # No package assesses this
+                              # "Disagreement of rep. meas."
+                              )) 
+
+# Color palette
+pal2 <- c("#0072B2", "#009E73", "#E69F00", "#D55E00")
+
+# Figure 4: Indicators by package ----
+dq_indicators <- pkgs_long %>% select(!c(Feature, Descriptors))
+
+pkgs_indicators <- dq_indicators %>% count(Dimension, Package)
+
+fig4 <- ggplot(pkgs_indicators, 
+               aes(x = Package, y = n, label = n, 
+                   fill = forcats::fct_rev(Dimension))) +
+  geom_bar(stat = "identity", 
+           color = "white",
+           width=1) +
+  geom_text(size = 4, position = position_stack(vjust = 0.5), 
+            colour = "white") +
+  scale_fill_manual(values = rev(pal2), 
+                    guide = guide_legend(reverse = TRUE),
+                    name = "Dimension") +  
+  coord_flip() +
+  scale_x_discrete(limits = rev(levels(pkgs_indicators$Package))) +
+  scale_y_continuous(n.breaks = 6, limits = c(0, 10),
+                     expand = expansion(add = c(0, .1))) +
+  labs(y = "No. of Domains with Data Quality Indicators")  +
+  theme_classic() +
+  theme(aspect.ratio = 1.1, 
+        axis.line.y = element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_text(size=rel(1.6)),
+        axis.text.y=element_text(size=rel(1.8)),
+        axis.title.x=element_text(size=rel(1.7)), 
+        axis.title.y= element_blank(),
+        legend.position="top", 
+        legend.justification='right',
+        legend.title = element_text(size=rel(1.5)),
+        legend.text = element_text(size=rel(1.4))
+  ) 
+fig4
+
+
+# Figure 4: Descriptors by package ----
+pkgs_domains <- pkgs_long %>% count(Dimension, Package)
+
+
+
+
+fig4 <- ggplot(pkgs_domains, 
+               aes(x = Package, y = n, label = n, 
+                   fill = forcats::fct_rev(Dimension))) +
+  geom_bar(stat = "identity", 
+           color = "white",
+           width=1) +
+  geom_text(size = 4, position = position_stack(vjust = 0.5), 
+            colour = "white") +
+  scale_fill_manual(values = rev(pal2), 
+                    guide = guide_legend(reverse = TRUE),
+                    name = "Dimension") +  
+  coord_flip() +
+  scale_x_discrete(limits = rev(levels(pkgs_domains$Package))) +
+  scale_y_continuous(n.breaks = 6, limits = c(0, 10),
+                     expand = expansion(add = c(0, .1))) +
+  labs(y = "No. of Domains with Data Quality Indicators")  +
+  theme_classic() +
+  theme(aspect.ratio = 1.1, 
+        axis.line.y = element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_text(size=rel(1.6)),
+        axis.text.y=element_text(size=rel(1.8)),
+        axis.title.x=element_text(size=rel(1.7)), 
+        axis.title.y= element_blank(),
+        legend.position="top", 
+        legend.justification='right',
+        legend.title = element_text(size=rel(1.5)),
+        legend.text = element_text(size=rel(1.4))
+  ) 
+fig4
+
+
+# Export ----
+# ggsave("figs/fig4_v5.pdf", fig4, width = 14, height = 8.5, units = "in", dpi = 400)
+# ggsave("figs/fig5_v4.pdf", fig5, width = 14, height = 8.5, units = "in", dpi = 400)
 
 # Rename levels for plotting
-levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Accuracy"] <- "Acc"
-levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Completeness"] <- "Com"
-levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Consistency"] <- "Con"
-levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Integrity"] <- "Int"
+# levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Accuracy"] <- "Acc"
+# levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Completeness"] <- "Com"
+# levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Consistency"] <- "Con"
+# levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Integrity"] <- "Int"
+
+# Rename levels for plotting
+# levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Acc"] <- "Accuracy"
+# levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Com"] <- "Completeness"
+# levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Con"] <- "Consistency"
+# levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Int"] <- "Integrity"
 
 # levels(pkgs_long$Package)[levels(pkgs_long$Package) == "dataReporter1"] <- "Int"
 # levels(pkgs_long$Package)[levels(pkgs_long$Package) == "MOQA2"] <- "Int"
@@ -54,89 +153,24 @@ levels(pkgs_long$Dimension)[levels(pkgs_long$Dimension) == "Integrity"] <- "Int"
 
 
 # Color palette
-pal1 <- c("#083666", "#005191", "#9EB7E5", "#00B691", "#0C6F5A", "#F2CF63", "#E59100", "#F0433A", "#C9283E", "#820333", "#2E112D")
+# pal1 <- c("#083666", "#005191", "#9EB7E5", "#00B691", "#0C6F5A", "#F2CF63", "#E59100", "#F0433A", "#C9283E", "#820333", "#2E112D")
 
 # Figure 5: Indicators by package ----
-
-fig5 <- ggplot(pkgs_long, aes(x = Dimension, y = Count))+
-  geom_bar(
-    aes(fill = Domain), stat = "identity", color = "white",
-    # Keep all bars of equal width
-    position = position_dodge2(preserve = "single")
-    # position = "dodge2" #position_dodge(0.9)
-  ) +
-  facet_wrap(~ Package) +
-  scale_fill_manual(values = pal1) +  
-  labs(y = "No. of indicators")  +
-  theme_bw(base_size = 14.5) + 
-  theme(strip.background = element_rect(fill="white"),
-        axis.text.x = element_text(angle = 45, hjust=1),
-        panel.grid.minor = element_blank())  
+# 
+# fig5 <- ggplot(pkgs_long, aes(x = Dimension, y = Count))+
+#   geom_bar(
+#     aes(fill = Domain), stat = "identity", color = "white",
+#     # Keep all bars of equal width
+#     position = position_dodge2(preserve = "single")
+#     # position = "dodge2" #position_dodge(0.9)
+#   ) +
+#   facet_wrap(~ Package) +
+#   scale_fill_manual(values = pal1) +  
+#   labs(y = "No. of indicators")  +
+#   theme_bw(base_size = 14.5) + 
+#   theme(strip.background = element_rect(fill="white"),
+#         axis.text.x = element_text(angle = 45, hjust=1),
+#         panel.grid.minor = element_blank())  
 # fig4
 
 
-# Figure 5: Domains by package 
-pkgs_domains <- pkgs_long %>% count(Dimension, Package)
-
-# Rename levels for plotting
-levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Acc"] <- "Accuracy"
-levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Com"] <- "Completeness"
-levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Con"] <- "Consistency"
-levels(pkgs_domains$Dimension)[levels(pkgs_domains$Dimension) == "Int"] <- "Integrity"
-
-# Color palette
-pal2 <- c("#005191", "#00B691", "#E59100", "#C9283E")
-
-# Figure 4: Domains by package ----
-
-fig4 <- ggplot(pkgs_domains, 
-                aes(x = Package, y = n, label = n, 
-                    fill = forcats::fct_rev(Dimension))) +
-          geom_bar(stat = "identity", 
-                   color = "white",
-                   width=1) +
-          geom_text(size = 4, position = position_stack(vjust = 0.5), 
-                    colour = "white") +
-          scale_fill_manual(values = rev(pal2), 
-                            guide = guide_legend(reverse = TRUE),
-                            name = "Dimension") +  
-          coord_flip() +
-          scale_x_discrete(limits = rev(levels(pkgs_domains$Package))) +
-          scale_y_continuous(n.breaks = 6, limits = c(0, 10),
-                             expand = expansion(add = c(0, .1))) +
-          labs(y = "No. of Domains")  +
-    theme_classic() +
-    theme(aspect.ratio = 1.1, 
-          axis.line.y = element_blank(),
-          axis.ticks.y=element_blank(),
-          axis.text.x=element_text(size=rel(1.6)),
-          axis.text.y=element_text(size=rel(1.8)),
-          axis.title.x=element_text(size=rel(1.7)), 
-          axis.title.y= element_blank(),
-          legend.position="top", 
-          legend.justification='right',
-          legend.title = element_text(size=rel(1.5)),
-          legend.text = element_text(size=rel(1.4))
-          ) 
-fig4
-
-# Another option with facets
-# fig5a <- ggplot(pkgs_domains, aes(x = Dimension, y = n))+
-#   geom_bar(
-#     aes(fill = Dimension), stat = "identity", color = "white",
-#     # Keep all bars of equal width
-#     # position = position_dodge2(preserve = "single")
-#     position = "dodge2" #position_dodge(0.9)
-#   ) +
-#   facet_wrap(~ Package) +
-#   scale_fill_manual(values = pal2) +  
-#   labs(y = "No. of Domains")  +
-#   theme_bw(base_size = 14.5) + 
-#   theme(strip.background = element_rect(fill="white"), 
-#         panel.grid.minor = element_blank(),
-#         legend.position="none")
-# fig5a
-
-# Export ----
-ggsave("figs/fig4_v4.pdf", fig4, width = 14, height = 8.5, units = "in", dpi = 400)
-ggsave("figs/fig5_v4.pdf", fig5, width = 14, height = 8.5, units = "in", dpi = 400)
