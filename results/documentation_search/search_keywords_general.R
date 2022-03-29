1e543df26edd2cbf9bbd0b3d0dc713a9ce78ca58
Joany Mariño <jmarinocoron@mun.ca>
2022-03-29 14:55:10 +0200

    add script to search for keywords

---
+++ results/documentation_search/search_keywords_general.R
@@ -0,0 +1,42 @@
+check_package_key_word <- function(packages = c(), terms = c()) {
+  lapply(
+    setNames(nm = packages),
+    function(package) {
+      lapply(
+        setNames(nm = terms),
+        function(term) { # https://github.com/cran/dataquieR/search?q=mean
+          q <- sprintf(
+            "https://github.com/cran/%s/search?q=%s",
+            package,
+            utils::URLencode(term)
+          )
+          return(htmltools::pre(htmltools::a(href=q, target = "_blank", paste0(package, ":", term, "\n", collapse = ""))))
+          # q <- sprintf(
+          #   # "https://github.com/cran/%s/search?q=%s",
+          #   "https://api.github.com/search/code?q=repo:cran/%s+%s",
+          #   package,
+          #   utils::URLencode(term)
+          # )
+          # cat(sprintf("requesting %s...", dQuote(q)))
+          # r <- jsonlite::fromJSON(q)
+          # if (r$total_count > 0) {
+          #   if (r$incomplete_results) cat("There are more results")
+          #   lapply(r$items$git_url, function(git_url) {
+          #     x <- jsonlite::fromJSON(git_url)
+          #     rawToChar(jsonlite::base64_dec(x$content))
+          #   })
+          # }
+        }
+      )
+    }
+  )
+}
+
+# edit accordingly ####
+kw <- "unique, plausib, confor, verific, valid, temporal, integrity, believ"
+pk <- "assertable assertive assertr clickR DataExplorer DataExplorer dataquieR dataReporter DescTools dlookr DQAstats DQAstats ExPanDaR explore funModeling inspectdf IPDFileCheck MOQA mStats observer pointblank sanityTracker skimr SmartEDA StatMeasures summarytools testdat validate visdat xray"
+#####
+
+htmltools::save_html(check_package_key_word(strsplit(pk, "\\s+", perl = TRUE)[[1]],
+                                            strsplit(kw, "\\s*,\\s*", perl = TRUE)[[1]]), file = "/tmp/output.html")
+rstudioapi::viewer("/tmp/output.html")
